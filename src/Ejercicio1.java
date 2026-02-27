9248cbf1661b4e558381a146cf79017c201fd440
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-02-27 11:02:06 +0100

    repaso.

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        for (int i = 1; i <= 200; i++) {
+            if ((i %3 == 0 ) || (i %5 == 0 )){
+                contador++;
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("Hay un total de " + contador + " múltiplos");
+    }
 }
